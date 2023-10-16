eb18b51e8657de47080df1e4ebc31f781ff75ed0
Ebenezer Bamfo-Bediako <ebamfo9@gmail.com>
2023-10-16 18:37:05 +0000

    added funtionality to create table and set counter to 0 for each setup

--- crc-code/HttpTrigger3/__init__.py
+++ crc-code/HttpTrigger3/__init__.py
@@ -7,9 +7,27 @@ import json
 
 func.HttpResponse.mimetype = 'application/json'
 func.HttpResponse.charset = 'utf-8'
+
+my_entity = {
+            "PartitionKey": "pk-01",
+            "RowKey": "rk-01",
+            "CurrentCounter":0
+        }
     
 def main(req: func.HttpRequest) -> func.HttpResponse:
         with TableClient.from_connection_string(conn_str=os.environ['CONNECTION_STR'], table_name="ViewsCounter") as table:
+            
+            # Create a table in case it does not already exist
+            try:
+                table.create_table()
+            except HttpResponseError:
+                pass
+            
+            # Create entity incase it does not exist ie table rows and default values
+            try:
+                table.create_entity(entity=my_entity)
+            except ResourceExistsError:
+                pass
 
             #Get Entity
             updated_entity = table.get_entity(partition_key="pk-01", row_key="rk-01")
